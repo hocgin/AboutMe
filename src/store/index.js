c4652c98d2d0d7da668e4eafa7d9339a4aa1709d
hocgin <hocgin@gmail.com>
2017-03-14 22:29:59 +0800

    滚动动画

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,10 @@ import Vuex from 'vuex'
 import user from './modules/user'
 import icon from './modules/icon'
 import project from './modules/project'
+import vueTap from 'v-tap'
 
 Vue.use(Vuex)
+Vue.use(vueTap)
 
 export default new Vuex.Store({
   state: {
