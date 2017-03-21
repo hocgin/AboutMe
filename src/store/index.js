3ffa6b858c41d5be1f4025c90d37b9d07dc97d6b
hocgin <hocgin@gmail.com>
2017-03-21 15:51:26 +0800

    - 移除无用依赖

--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,8 @@ import Vuex from 'vuex'
 import user from './modules/user'
 import icon from './modules/icon'
 import project from './modules/project'
-import vueTap from 'v-tap'
 
 Vue.use(Vuex)
-Vue.use(vueTap)
 
 export default new Vuex.Store({
   state: {
