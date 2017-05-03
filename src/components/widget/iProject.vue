9795913faa78a439587a9602a2055ca0cbca896c
hocgin <hocgin@gmail.com>
2017-05-03 14:57:16 +0800

    - 加入了动画 - 增加了下拉 - 优化了位置关系

--- src/components/widget/iProject.vue
+++ src/components/widget/iProject.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="iProject">
     <div class="photo">
-      <img :src="_photo" data-src="holder.js/50x50?text=Logo" alt="">
+      <img :src="_photo" data-src="holder.js/50x50?text=Logo" alt="头像"/>
       <div class="info">
         <p class="name">{{ _title }}</p>
         <p class="version">{{ _version }}</p>
@@ -9,34 +9,6 @@
     </div>
   </div>
 </template>
-<style lang="scss" scoped>
-  .iProject {
-      margin-top: .9em;
-      .photo {
-          img {
-            border: solid 1px #eee;
-            width: 50px;
-            height: 50px;
-            border-radius: 50%;
-            box-shadow: 1px 1px 1px rgba(0,0,0,.3);
-          }
-          .info {
-            display: inline-block;
-            position: relative;
-            top: -10px;
-            margin-left: .4em;
-            .name {
-              font-weight: bolder;
-            }
-            .version {
-              font-size: .7em;
-              text-align: left;
-              color: #96948f;
-            }
-          }
-      }
-  }
-</style>
 <script>
   export default {
     name: 'iProject',
@@ -55,3 +27,39 @@
     }
   }
 </script>
+<style lang="scss" scoped>
+  .iProject {
+    margin-top: .9em;
+      .photo {
+          img {
+            border: solid 1px #eee;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            box-shadow: 1px 1px 1px rgba(0, 0, 0, .3);
+
+            &:hover {
+               animation: 1s rubberBand forwards;
+             }
+          }
+
+          .info {
+            display: inline-block;
+            position: relative;
+            top: -10px;
+            margin-left: .4em;
+
+              .name {
+                font-weight: bolder;
+              }
+
+              .version {
+                font-size: .7em;
+                text-align: left;
+                color: #96948f;
+              }
+
+          }
+      }
+  }
+</style>
