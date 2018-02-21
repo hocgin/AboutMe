3066f615a9fc912a979195e60f6a2067c86a93cc
hocgin <hocgin@gmail.com>
2018-02-21 15:25:14 +0800

    -updated ;

--- src/App.vue
+++ src/App.vue
@@ -1,109 +1,109 @@
 <template>
-  <div id="app" class="animated" :class="animateInClass">
-    <div class="app-box">
-      <iHeader :_nick="info.nick" :_subtitle="info.subtitle" :_brief="info.brief" :_photo="info.photo"></iHeader>
-      <transition
-        enter-active-class="animated bounceIn"
-      >
-        <div v-if="showMore">
-          <iHr _color="#CBCBCB"></iHr>
-          <iPanel></iPanel>
+    <div id="app" class="animated" :class="animateInClass">
+        <div class="app-box">
+            <iHeader :_nick="info.nick" :_subtitle="info.subtitle" :_brief="info.brief" :_photo="info.photo"></iHeader>
+            <transition
+                    enter-active-class="animated bounceIn"
+            >
+                <div v-if="showMore">
+                    <iHr _color="#CBCBCB"></iHr>
+                    <iPanel></iPanel>
+                </div>
+            </transition>
         </div>
-      </transition>
+        <a @click.prevent="showMoreClick">
+            <iMore :_hidden="showMore"></iMore>
+        </a>
     </div>
-    <a @click.prevent="showMoreClick">
-      <iMore :_hidden="showMore"></iMore>
-    </a>
-  </div>
 </template>
 
 <script>
-  import iHr from '@/components/widget/iHr'
-  import iHeader from '@/components/widget/iHeader'
-  import iPanel from '@/components/iPanel'
-  import iMore from '@/components/widget/iMore'
-  import $ from 'jquery'
+    import iHr from '@/components/widget/iHr'
+    import iHeader from '@/components/widget/iHeader'
+    import iPanel from '@/components/iPanel'
+    import iMore from '@/components/widget/iMore'
+    import $ from 'jquery'
 
-  export default {
-    name: 'app',
-    components: {
-      iHr,
-      iHeader,
-      iPanel,
-      iMore
-    },
-    methods: {
-      showMoreClick: function (event) {
-        this.showMore = !this.showMore
-        if (this.showMore) {
-          $('#app').animate({'margin-top': '4em'})
-        } else {
-          $('#app').animate({'margin-top': '12em'})
+    export default {
+        name: 'app',
+        components: {
+            iHr,
+            iHeader,
+            iPanel,
+            iMore
+        },
+        methods: {
+            showMoreClick: function (event) {
+                this.showMore = !this.showMore;
+                if (this.showMore) {
+                    $('#app').animate({'margin-top': '4em'})
+                } else {
+                    $('#app').animate({'margin-top': '12em'})
+                }
+            }
+        },
+        data() {
+            return {
+                info: this.$store.state.user,
+                showMore: false,
+                animateInClass: [
+                    'flipInY',
+                    'flipInX',
+                    'bounceIn',
+                    'fadeIn',
+                    'rotateIn',
+                    'rollIn',
+                    'zoomIn'
+                ][Math.floor(Math.random() * 7)]
+            }
         }
-      }
-    },
-    data () {
-      return {
-        info: this.$store.state.user,
-        showMore: false,
-        animateInClass: [
-          'flipInY',
-          'flipInX',
-          'bounceIn',
-          'fadeIn',
-          'rotateIn',
-          'rollIn',
-          'zoomIn'
-        ][Math.floor(Math.random() * 7)]
-      }
     }
-  }
 
-  // 切换标题
-  let c
-  let u = document.title
-  document.addEventListener('visibilitychange', function () {
-    document.hidden ? ($('[rel="shortcut icon"]').attr('href', 'https://hocg.in/fail.ico'),
-        document.title = '(●—●)喔哟，崩溃啦！',
-        clearTimeout(c)) : ($('[rel="shortcut icon"]').attr('href', 'https://hocg.in/favicon.ico?v=0.5.0'),
-        document.title = '(/≧▽≦/)咦！又好了！' + u,
-        c = setTimeout(function () {
-          document.title = u
-        }, 2e3))
-  })
+    // 切换标题
+    let c,
+        u = document.title;
+    document.addEventListener('visibilitychange', function () {
+        document.hidden ? ($('[rel="shortcut icon"]').attr('href', 'https://hocg.in/fail.ico'), document.title = '(●—●)喔哟，崩溃啦！', clearTimeout(c))
+            : ($('[rel="shortcut icon"]').attr('href', 'https://hocg.in/favicon.ico?v=0.5.0'),
+                document.title = ('(/≧▽≦/)咦！又好了！' + u),
+                c = setTimeout(function () {
+                    document.title = u;
+                }, 2e3));
+    });
 </script>
 
 <style lang="scss">
-  * {
-    font-family: "Avenir Next", Helvetica, Arial, "Lantinghei SC", "Microsoft YaHei", sans-serif;
-  }
-  #app {
+    * {
+        font-family: "Avenir Next", Helvetica, Arial, "Lantinghei SC", "Microsoft YaHei", sans-serif;
+    }
 
-    /*max-width: 335px;*/
-    max-width: 410px;
-    margin: 10em auto 1em;
+    #app {
 
-  /*margin: 0 auto;*/
+        /*max-width: 335px;*/
+        max-width: 410px;
+        margin: 10em auto 1em;
 
-  .app-box {
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    box-shadow: 0 0 4px #e8eaeb;
-    border-radius: 2px;
-    padding-bottom: .45em;
-    overflow: hidden;
-    /*min-width: 250px;*/
-    background-color: #fff;
-  }
+        /*margin: 0 auto;*/
 
-  }
+        .app-box {
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+            text-align: center;
+            color: #2c3e50;
+            box-shadow: 0 0 4px #e8eaeb;
+            border-radius: 2px;
+            padding-bottom: .45em;
+            overflow: hidden;
+            /*min-width: 250px;*/
+            background-color: #fff;
+        }
 
-  * {
-    margin: 0;
-    padding: 0;
-    cursor: url('https://hocg.in/mouse.png'), auto;
-  }
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        cursor: url('https://hocg.in/mouse.png'), auto;
+    }
 
 </style>
--- src/api/icon.js
+++ src/api/icon.js
@@ -1,85 +1,85 @@
 const _data = [
-  {
-    'icon': '<i class="fa fa-quote-left fa-lg"></i>',
-    'href': 'http://hocg.in',
-    'title': '-'
-  },
-  {
-    'icon': '<i class="fa fa-code fa-lg animate-rotate-340"></i>',
-    'href': 'http://hocg.in',
-    'title': '博客'
-  },
-  {
-    'icon': '<i class="fa fa-github fa-lg animate-rotate-340"></i>',
-    'href': 'http://www.github.com/hocgin',
-    'title': 'GitHub'
-  },
-  {
-    'icon': '<i class="fa fa-map-marker fa-lg transition-sk"></i>',
-    'href': 'https://www.google.com/maps/place/%E5%8E%A6%E9%97%A8%E7%90%86%E5%B7%A5%E5%AD%A6%E9%99%A2/@24.4027969,117.980047,11z/data=!4m8!1m2!2m1!1z5Y6m6Zeo55CG5bel5a2m6ZmiIOmbhue-juWMug!3m4!1s0x34148e739374bb4b:0xed1c2bdf2656a98a!8m2!3d24.620539!4d118.084631?hl=zh-CN',
-    'title': '坐标'
-  },
-  {
-    'icon': '<i class="fa fa-weibo fa-lg animate-rotate-340"></i>',
-    'href': 'http://www.sina.com/hocgin',
-    'title': '微博'
-  },
-  {
-    'icon': '<i class="fa animate-rotate-340" style="font-weight: 500">知</i>',
-    'href': 'https://www.zhihu.com/people/hocgin',
-    'title': '知乎'
-  },
-  {
-    'icon': '<i class="fa fa-500px fa-lg animate-rotate-340"></i>',
-    'href': 'https://500px.com/hocgin',
-    'title': '500px'
-  },
-  {
-    'icon': '<i class="fa animate-rotate-340">简</i>',
-    'href': 'http://www.jianshu.com/users/f54746265931/timeline',
-    'title': '简书'
-  },
-  {
-    'icon': '<i class="fa fa-google-plus fa-lg animate-rotate-340"></i>',
-    'href': 'https://plus.google.com/u/hocgin',
-    'title': 'Google+'
-  },
-  {
-    'icon': '<i class="fa fa-wechat fa-lg animate-rotate-340"></i>',
-    'href': 'http://7xs6lq.com1.z0.glb.clouddn.com/img/un/qq.group.jpg',
-    'title': '交流群'
-  },
-  {
-    'icon': '<i class="fa fa-qq fa-lg animate-rotate-340"></i>',
-    'href': 'tencent://message/?uin=578797748',
-    'title': 'QQ'
-  },
-  {
-    'icon': '<i class="fa fa-envelope fa-lg animate-rotate-340"></i>',
-    'href': 'mailto:hocgin@gmail.co?subject=来自博客',
-    'title': '邮箱'
-  },
-  {
-    'icon': '<i class="animate-rotate-340">简历</i>',
-    'href': 'http://vita.hocg.in/',
-    'title': '简历'
-  },
-  {
-    'icon': '<i class="fa fa-language fa-lg animate-rotate-340"></i>',
-    'href': 'http://hocg.in/language',
-    'title': '翻译计划'
-  },
-  {
-    'icon': '<i class="fa fa-rss fa-lg animate-rotate-340"></i>',
-    'href': 'http://hocg.in/atom.xml',
-    'title': 'RSS'
-  },
-  {
-    'icon': '<i class="fa fa-quote-right fa-lg"></i>',
-    'href': 'http://hocg.in',
-    'title': '-'
-  }
-]
+    {
+        'icon': '<i class="fa fa-quote-left fa-lg"></i>',
+        'href': 'http://hocg.in',
+        'title': '-'
+    },
+    {
+        'icon': '<i class="fa fa-code fa-lg animate-rotate-340"></i>',
+        'href': 'http://hocg.in',
+        'title': '博客'
+    },
+    {
+        'icon': '<i class="fa fa-github fa-lg animate-rotate-340"></i>',
+        'href': 'http://www.github.com/hocgin',
+        'title': 'GitHub'
+    },
+    {
+        'icon': '<i class="fa fa-map-marker fa-lg transition-sk"></i>',
+        'href': 'https://www.google.com/maps/place/%E5%8E%A6%E9%97%A8%E7%90%86%E5%B7%A5%E5%AD%A6%E9%99%A2/@24.4027969,117.980047,11z/data=!4m8!1m2!2m1!1z5Y6m6Zeo55CG5bel5a2m6ZmiIOmbhue-juWMug!3m4!1s0x34148e739374bb4b:0xed1c2bdf2656a98a!8m2!3d24.620539!4d118.084631?hl=zh-CN',
+        'title': '坐标'
+    },
+    {
+        'icon': '<i class="fa fa-weibo fa-lg animate-rotate-340"></i>',
+        'href': 'http://www.sina.com/hocgin',
+        'title': '微博'
+    },
+    {
+        'icon': '<i class="fa animate-rotate-340" style="font-weight: 500">知</i>',
+        'href': 'https://www.zhihu.com/people/hocgin',
+        'title': '知乎'
+    },
+    {
+        'icon': '<i class="fa fa-500px fa-lg animate-rotate-340"></i>',
+        'href': 'https://500px.com/hocgin',
+        'title': '500px'
+    },
+    {
+        'icon': '<i class="fa animate-rotate-340">简</i>',
+        'href': 'http://www.jianshu.com/users/f54746265931/timeline',
+        'title': '简书'
+    },
+    {
+        'icon': '<i class="fa fa-google-plus fa-lg animate-rotate-340"></i>',
+        'href': 'https://plus.google.com/u/hocgin',
+        'title': 'Google+'
+    },
+    {
+        'icon': '<i class="fa fa-wechat fa-lg animate-rotate-340"></i>',
+        'href': 'http://7xs6lq.com1.z0.glb.clouddn.com/img/un/qq.group.jpg',
+        'title': '交流群'
+    },
+    {
+        'icon': '<i class="fa fa-qq fa-lg animate-rotate-340"></i>',
+        'href': 'tencent://message/?uin=578797748',
+        'title': 'QQ'
+    },
+    {
+        'icon': '<i class="fa fa-envelope fa-lg animate-rotate-340"></i>',
+        'href': 'mailto:hocgin@gmail.co?subject=来自博客',
+        'title': '邮箱'
+    },
+    {
+        'icon': '<i class="animate-rotate-340">简历</i>',
+        'href': 'https://hocg.in/Resume/',
+        'title': '简历'
+    },
+    {
+        'icon': '<i class="fa fa-language fa-lg animate-rotate-340"></i>',
+        'href': 'http://hocg.in/language',
+        'title': '翻译计划'
+    },
+    {
+        'icon': '<i class="fa fa-rss fa-lg animate-rotate-340"></i>',
+        'href': 'http://hocg.in/atom.xml',
+        'title': 'RSS'
+    },
+    {
+        'icon': '<i class="fa fa-quote-right fa-lg"></i>',
+        'href': 'http://hocg.in',
+        'title': '-'
+    }
+];
 
 export default {
   get () {
